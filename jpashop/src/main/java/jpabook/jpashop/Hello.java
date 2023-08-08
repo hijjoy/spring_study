08ed68e25d65e62b522dc04124e8ebf142af0fe7
hijjoy <hijjoy@naver.com>
2023-08-08 17:40:33 +0900

    [spring-boot & jpa] 기본 설정

---
+++ jpashop/src/main/java/jpabook/jpashop/Hello.java
@@ -0,0 +1,11 @@
+package jpabook.jpashop;
+
+import lombok.Getter;
+import lombok.Setter;
+
+
+// lombok 이용하면 따로 Getter Setter 안만들어줘도 됨 !
+@Getter @Setter
+public class Hello {
+    private String data;
+}
